e62c398bdf4fa9df37d5a7e2e3a585164471a8a6
idaoyu <3151029454@qq.com>
2022-12-10 20:27:45 +0800

    优化依赖、项目配置

--- src/main/java/cn/idaoyu/project/DaoyuApplication.java
+++ src/main/java/cn/idaoyu/project/DaoyuApplication.java
@@ -3,11 +3,17 @@ package cn.idaoyu.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 一条秋刀鱼zz
+ * @className DaoyuApplication
+ * @description Spring Boot 启动类
+ * @date 2022/12/10 20:24
+ */
 @SpringBootApplication
-public class DaoyuSpringBootTemplateApplication {
+public class DaoyuApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DaoyuSpringBootTemplateApplication.class, args);
+        SpringApplication.run(DaoyuApplication.class, args);
     }
 
 }
--- src/test/java/cn/idaoyu/project/DaoyuApplicationTests.java
+++ src/test/java/cn/idaoyu/project/DaoyuApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DaoyuSpringBootTemplateApplicationTests {
+class DaoyuApplicationTests {
 
     @Test
     void contextLoads() {
