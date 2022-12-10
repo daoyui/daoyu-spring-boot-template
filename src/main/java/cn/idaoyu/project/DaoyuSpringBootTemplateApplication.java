58501f48b4de787e07dcdc47529ff1a1a186bb83
idaoyu <3151029454@qq.com>
2022-12-10 17:58:18 +0800

    Initial commit

---
+++ src/main/java/cn/idaoyu/project/DaoyuSpringBootTemplateApplication.java
@@ -0,0 +1,13 @@
+package cn.idaoyu.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoyuSpringBootTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaoyuSpringBootTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/idaoyu/project/DaoyuSpringBootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.idaoyu.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaoyuSpringBootTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
